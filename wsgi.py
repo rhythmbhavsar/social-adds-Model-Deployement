9204acd43c67b0bf709562ba2df6acaa01e4229a
Rhythm Bhavsar <98228696+rhythmbhavsar@users.noreply.github.com>
2023-01-20 14:48:36 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/root/customer-account-automation/")
-
-from app import app as application
-print("run")
-app = application
-print("end")
